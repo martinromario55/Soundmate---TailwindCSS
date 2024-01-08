e30ad204118ac5d223265b59f10c02c53a3c6698
martinromario55 <martinromario55@gmail.com>
2024-01-08 06:43:34 +0300

    Tailwindcss tutorial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Soundmate---TailwindCSS
---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "logo": {
+          700: "#1C59AE",
+          800: "#114A9A",
+        },
+      },
+    },
+  },
+  darkMode: 'class',
+}
